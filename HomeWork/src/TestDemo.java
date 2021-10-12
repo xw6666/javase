ea5045232dcb8ab39434ec3c11409f4589105008
xw6666 <1481016131@qq.com>
2021-10-12 23:26:30 +0800

    更新java作业

--- HomeWork/src/TestDemo.java
+++ HomeWork/src/TestDemo.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 //public class TestDemo {
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
@@ -115,3 +115,100 @@
 //        }
 //    }
 //}
+
+//public class TestDemo {
+//    public static void printTriangle(int n) {
+//        for(int i = 0;i < n;i++) {
+//            //打印空格
+//            for(int j = 0;j < n - 1 - i;j++) {
+//                System.out.print(" ");
+//            }
+//            //打印符号
+//            for(int j = 0;j < 2 * i + 1;j++) {
+//                System.out.print("*");
+//            }
+//            //换行
+//            System.out.println();
+//        }
+//    }
+//
+//    public static void printDiamond(int n) {
+//        //打印上半部分
+//        printTriangle(n);
+//        //打印下半部分
+//        for(int i = 0;i < n - 1;i++) {
+//            //打印空格
+//            for(int j = 0;j < 1 + i;j++) {
+//                System.out.print(" ");
+//            }
+//            //打印符号
+//            for(int j = 0;j < 2 * n- 2 *i - 3;j++) {
+//                System.out.print("*");
+//            }
+//            //打印换行
+//            System.out.println();
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("请输入要打印的行数:>");
+//        do {
+//            int n = scanner.nextInt();
+//            printTriangle(n);
+//            System.out.println("----------------------这是分割线----------------------");
+//            printDiamond(n);
+//            System.out.print("请输入要打印的行数:>");
+//        }while(scanner.hasNext());
+//
+//    }
+//}
+
+//public class TestDemo {
+//
+//    /**
+//     * 冒泡排序
+//     * @param arr
+//     */
+//    public static void bubbleSort(int[] arr) {
+//        for(int i = 0;i < arr.length - 1;i++) {
+//            int flag = 1;   //flag = 1表示假设数组元素已经有序
+//            for(int j = 0;j < arr.length - i - 1;j++) {
+//                if(arr[j] > arr[j + 1])
+//                {
+//                    flag = 0;  //无序 需要交换
+//                    int temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//            if(flag == 1) {
+//                break;
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        int arr[] = new int[] {5,9,1,4,7,3,2,8,0,6};
+//        System.out.println("排序前：" + Arrays.toString(arr));
+//        bubbleSort(arr);
+//        System.out.println("排序后：" + Arrays.toString(arr));
+//    }
+//}
+
+
+public class TestDemo {
+
+    public static void print() {
+        for(int i = 1;i <= 9;i++) {
+            for(int j = 1;j <= i;j++) {
+                System.out.printf("%d * %d = %d\t", j, i, i*j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
